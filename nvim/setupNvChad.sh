5df7999f0e183c3fe90cedd420bda81639991c33
Robert Hunt <rhunt@live.com>
2024-02-27 12:57:46 -0800

    Adding setup for NVChad

---
+++ nvim/setupNvChad.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+echo "Installing NVChad"
+
+echo "Installing necessary environment packages..."
+if [ ! -f /usr/bin/nala ]; then
+	echo "Installing nala. It's better than apt... Use it instead :)"
+	sudo apt update && sudo apt install -y nala
+fi
+
+echo "Installing dependencies"
+sudo nala install -y ripgrep build-essential fd-find zip unzip
+sudo snap install lazygit
+
+echo "Installing neovim"
+sudo snap install nvim --classic
+
+echo "Backing up old nvim setup"
+mv ~/.config/nvim{,.bak}
+
+# optional but recommended
+mv ~/.local/share/nvim{,.bak}
+mv ~/.local/state/nvim{,.bak}
+mv ~/.cache/nvim{,.bak}
+
+# Install the NVChad starter
+git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1 && $(NVCHAD_EXAMPLE_CONFIG=y nvim --headless "+q") 
+
+# Remove the git folder, so it can be added to my own custom repo maybe
+rm -rf ~/.config/nvim/.git
+
+echo "Done!"
