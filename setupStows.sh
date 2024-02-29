b0d1a2ecd22dd1332475d467da118da48baeda3d
Robert Hunt <rhunt@live.com>
2024-02-29 11:21:39 -0800

    Moving the setup for tmuxinator into setupStows, since it is more generic, and makes it obvious the folder is being made, and other things can count on it being done.

--- setupStows.sh
+++ setupStows.sh
@@ -8,8 +8,12 @@ fi
 
 if [ ! -f /usr/bin/stow ]; then
 	echo "Installing stow, this will be used to manage the symlinks to the dotfiles dir..."
-	sudo nala update && sudo nala install -y --no-install-recommends stow 
+	sudo nala update && sudo nala install -y --no-install-recommends stow
 fi
+
+# Ensure the $HOME/.config folder exists so stow doesn't try to link the entire thing.
+# This helps with other stows wanting to put things in this same folder tree.
+mkdir -p $HOME/.config/
 
 # Git needs some special attention, as the ~/.gitconfig might already exist, and we don't want to overwrite it.
 pushd git
@@ -17,18 +21,14 @@ pushd git
 popd
 
 # Tmuxinator needs some extra attention. We need to ensure the $HOME/.config/tmuxinator folder is already existing.
-pushd tmuxinator
-./setupTmuxinator.sh
-popd
+mkdir -p $HOME/.config/tmuxinator
 
 # Ignore anything that starts with setup, or ends with _OLD, _NEW, etc.
 ignorePattern="^setup.*\.sh|.*_OLD$|.*_NEW$|.*_BASE$"
 echo "Installing symlinks for .dotfiles into the $HOME folder..."
-stow --verbose=1 --ignore=$ignorePattern --target=$HOME aliases bash git omz tmux tmuxinator vim 
-
+stow --verbose=1 --ignore=$ignorePattern --target=$HOME aliases bash git omz tmux tmuxinator vim
 
 echo "Adding a line to ~/.bashrc to execute our extra stuff..."
-echo -e "if [ -f $HOME/.bashrc_plus ]; then\n\tsource $HOME/.bashrc_plus\nfi\n" >> $HOME/.bashrc
+echo -e "if [ -f $HOME/.bashrc_plus ]; then\n\tsource $HOME/.bashrc_plus\nfi\n" >>$HOME/.bashrc
 
 echo "Done!"
-
--- tmuxinator/setupTmuxinator.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Ensure the $HOME/.config/tmuxinator folder exists so stow doesn't try to link the entire thing. 
-# This helps with other stows wanting to put things in this same folder tree.
-mkdir -p $HOME/.config/tmuxinator
-
-
