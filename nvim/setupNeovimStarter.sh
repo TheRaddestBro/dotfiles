f5c6713c14b58c6f0cb73b4ccdf5b615f3e5b4e6
Robert Hunt <rhunt@live.com>
2024-02-29 11:24:54 -0800

    Adding config possibility for NvChad and NV Kickstart

--- nvim/setupNeovimStarter.sh
+++ nvim/setupNeovimStarter.sh
@@ -25,8 +25,15 @@ mv ~/.cache/nvim{,.bak}
 # Install the Official Neovim kickstarter
 git clone https://github.com/nvim-lua/kickstart.nvim.git "${XDG_CONFIG_HOME:-$HOME/.config}"/nvim
 
+# TODO: We need to ensure the config folder is available, so we don't stow the wrong thing
+#mkdir -p $HOME/.config/nvim/lua/plugins
+
 # Remove the git folder, so it can be added to my own custom repo maybe
 rm -rf ~/.config/nvim/.git
 
-echo "Done!"
+# TODO: Fill this out the next time I actually try out the NV Starter
+#ignorePattern="^setup.*\.sh|.*_OLD$|.*_NEW$|.*_BASE$"
+#echo "Installing symlinks for .dotfiles into the $HOME folder..."
+#stow --verbose=1 --ignore=$ignorePattern --target=$HOME nvimStarter
 
+echo "Done!"
--- nvim/setupNvChad.sh
+++ nvim/setupNvChad.sh
@@ -23,9 +23,17 @@ mv ~/.local/state/nvim{,.bak}
 mv ~/.cache/nvim{,.bak}
 
 # Install the NVChad starter
-git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1 && $(NVCHAD_EXAMPLE_CONFIG=y nvim --headless "+q") 
+git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1 && $(NVCHAD_EXAMPLE_CONFIG=y nvim --headless "+q")
+
+# TODO: We need to ensure the config folder is available, so we don't stow the wrong thing
+#mkdir -p $HOME/.config/nvim/lua/plugins
 
 # Remove the git folder, so it can be added to my own custom repo maybe
 rm -rf ~/.config/nvim/.git
+
+# TODO: Fill this out the next time I actually try out NvChad
+#ignorePattern="^setup.*\.sh|.*_OLD$|.*_NEW$|.*_BASE$"
+#echo "Installing symlinks for .dotfiles into the $HOME folder..."
+#stow --verbose=1 --ignore=$ignorePattern --target=$HOME nvChad
 
 echo "Done!"
