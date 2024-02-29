ba66812783a30c8fdc5aa3724c7ed51abd5fe851
Robert Hunt <rhunt@live.com>
2024-02-29 11:21:53 -0800

    Adding LazyVim configs

--- nvim/setupLazyVim.sh
+++ nvim/setupLazyVim.sh
@@ -24,7 +24,14 @@ mv ~/.cache/nvim{,.bak}
 # Install the LazyVim starter
 git clone https://github.com/LazyVim/starter ~/.config/nvim
 
+# We need to ensure the $HOME/.config/nvim/lua/plugins folder is already existing.
+mkdir -p $HOME/.config/nvim/lua/plugins
+
 # Remove the git folder, so it can be added to my own custom repo maybe
 rm -rf ~/.config/nvim/.git
+
+ignorePattern="^setup.*\.sh|.*_OLD$|.*_NEW$|.*_BASE$"
+echo "Installing symlinks for .dotfiles into the $HOME folder..."
+stow --verbose=1 --ignore=$ignorePattern --target=$HOME lazyvim
 
 echo "Done!"
