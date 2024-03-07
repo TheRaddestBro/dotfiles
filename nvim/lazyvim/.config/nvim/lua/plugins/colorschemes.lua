32489a2cdc9d4f31e41a0197866b7673b222eeed
Robert Hunt <rhunt@live.com>
2024-03-07 15:39:48 -0800

    Renaming colorscheme.lua to colorschemes.lua to make it more obvious that it contains many schemes

---
+++ nvim/lazyvim/.config/nvim/lua/plugins/colorschemes.lua
@@ -0,0 +1,19 @@
+return {
+  -- Add colorschemes
+  { "rktjmp/lush.nvim" },
+  { "ellisonleao/gruvbox.nvim" },
+  { "cryptomilk/nightcity.nvim", version = false },
+  { "metalelf0/jellybeans-nvim" },
+  { "casonadams/walh" },
+  { "ntk148v/habamax.nvim" },
+  { "uloco/bluloco.nvim" },
+  --{ "catppucin/nvim" },
+
+  -- Configure default
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin-mocha",
+    },
+  },
+}
