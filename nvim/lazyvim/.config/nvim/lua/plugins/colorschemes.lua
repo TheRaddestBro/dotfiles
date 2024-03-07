a99f9da2de8ef5ddf69f95ae7babeadf30e51ca2
Robert Hunt <rhunt@live.com>
2024-03-07 15:49:42 -0800

    Marking all of the color schemes I am not using as lazy load

--- nvim/lazyvim/.config/nvim/lua/plugins/colorschemes.lua
+++ nvim/lazyvim/.config/nvim/lua/plugins/colorschemes.lua
@@ -1,15 +1,16 @@
 return {
   -- Add colorschemes
   -- Lush is used by several of the below schemes
-  { "rktjmp/lush.nvim", Lazy = false },
-  --{ "ellisonleao/gruvbox.nvim" },
-  --{ "cryptomilk/nightcity.nvim", version = false },
-  { "metalelf0/jellybeans-nvim" },
-  --{ "casonadams/walh" },
-  --{ "ntk148v/habamax.nvim" },
-  --{ "uloco/bluloco.nvim" },
-  --{ "tokyonight/nvim" },
-  --{ "catppucin/nvim" },
+  { "rktjmp/lush.nvim", lazy = false },
+  { "ellisonleao/gruvbox.nvim", lazy = true },
+  { "cryptomilk/nightcity.nvim", lazy = true, version = false },
+  { "metalelf0/jellybeans-nvim", lazy = false },
+  { "casonadams/walh", lazy = true },
+  { "ntk148v/habamax.nvim", lazy = true },
+  { "uloco/bluloco.nvim", lazy = true },
+  -- These seem to already be included in LazyVim, leaving them here for posterity
+  --{ "folke/tokyonight.nvim", lazy = true },
+  --{ "catppuccin/nvim" },
 
   -- Configure default
   {
