37af0110212bfee7a5822ba07cb1fdd6b854cca4
Robert Hunt <rhunt@live.com>
2024-03-07 15:39:38 -0800

    Cleaning up colorschemes.lua a bit. No need to load these schemes I probably won't use right now

--- nvim/lazyvim/.config/nvim/lua/plugins/colorschemes.lua
+++ nvim/lazyvim/.config/nvim/lua/plugins/colorschemes.lua
@@ -1,19 +1,21 @@
 return {
   -- Add colorschemes
+  -- Lush is used by several of the below schemes
   { "rktjmp/lush.nvim", Lazy = false },
-  { "ellisonleao/gruvbox.nvim" },
-  { "cryptomilk/nightcity.nvim", version = false },
+  --{ "ellisonleao/gruvbox.nvim" },
+  --{ "cryptomilk/nightcity.nvim", version = false },
   { "metalelf0/jellybeans-nvim" },
-  { "casonadams/walh" },
-  { "ntk148v/habamax.nvim" },
-  { "uloco/bluloco.nvim" },
+  --{ "casonadams/walh" },
+  --{ "ntk148v/habamax.nvim" },
+  --{ "uloco/bluloco.nvim" },
+  { "tokyonight/nvim" },
   --{ "catppucin/nvim" },
 
   -- Configure default
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "catppuccin-mocha",
+      colorscheme = "tokyonight-night",
     },
   },
 }
