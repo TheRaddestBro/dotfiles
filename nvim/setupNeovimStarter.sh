5fa31cc34cd5c765a08df4eb5db64391fe4daab1
Robert Hunt <rhunt@live.com>
2024-03-07 15:39:53 -0800

    Adding plugins setup for nvim kickstart

--- nvim/setupNeovimStarter.sh
+++ nvim/setupNeovimStarter.sh
@@ -9,7 +9,6 @@ fi
 
 echo "Installing dependencies"
 sudo nala install -y build-essential
-sudo snap install lazygit
 
 echo "Installing neovim"
 sudo snap install nvim --classic
@@ -26,15 +25,15 @@ mv ~/.cache/nvim{,.bak}
 git clone https://github.com/nvim-lua/kickstart.nvim.git "${XDG_CONFIG_HOME:-$HOME/.config}"/nvim
 
 # TODO: We need to ensure the config folder is available, so we don't stow the wrong thing
-#mkdir -p $HOME/.config/nvim/lua/plugins
+mkdir -p $HOME/.config/nvim/lua/custom/plugins
 
 # Remove the git folder, so it can be added to my own custom repo maybe
 rm -rf ~/.config/nvim/.git
 rm -rf ~/.config/nvim/.github
 
 # TODO: Fill this out the next time I actually try out the NV Starter
-#ignorePattern="^setup.*\.sh|.*_OLD$|.*_NEW$|.*_BASE$"
-#echo "Installing symlinks for .dotfiles into the $HOME folder..."
-#stow --verbose=1 --ignore=$ignorePattern --target=$HOME nvimStarter
+ignorePattern="^setup.*\.sh|.*_OLD$|.*_NEW$|.*_BASE$"
+echo "Installing symlinks for .dotfiles into the $HOME folder..."
+stow --verbose=1 --ignore=$ignorePattern --target=$HOME kickstart
 
 echo "Done!"
