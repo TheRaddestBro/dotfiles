5c5201099b8d276e047d11cd5abbbcabe714b322
Robert Hunt <rhunt@CHUNGUS.localdomain>
2025-11-19 23:45:17 -0800

    Fixing typo in comments

--- _justTheStows.sh
+++ _justTheStows.sh
@@ -2,16 +2,16 @@
 
 echo "Ensuring dependencies are available for stows..."
 if [ ! -f /usr/bin/nala ]; then
-	echo "Installing nala. It's superior to apt in every way. Use it instead! :)"
-	sudo apt update && sudo apt install -y nala
+  echo "Installing nala. It's superior to apt in every way. Use it instead! :)"
+  sudo apt update && sudo apt install -y nala
 fi
 
 if [ ! -f /usr/bin/stow ]; then
-	echo "Installing stow, this will be used to manage the symlinks to the dotfiles dir..."
-	sudo nala update && sudo nala install -y --no-install-recommends stow
+  echo "Installing stow, this will be used to manage the symlinks to the dotfiles dir..."
+  sudo nala update && sudo nala install -y --no-install-recommends stow
 fi
 
-# Make subfolders to we can ensure we don't stow the parent folders we don't want.
+# Make subfolders so we can ensure we don't stow the parent folders we don't want.
 mkdir -p $HOME/.config/tmuxinator
 
 # Ignore anything that starts with setup, or ends with _OLD, _NEW, etc.
