bf45e91390b757786f766ae50d0953f3473970ad
Robert Hunt <rhunt@live.com>
2026-01-28 10:44:18 -0800

    Adding fedora checking to _getDistro.sh

--- helpers/_getDistro.sh
+++ helpers/_getDistro.sh
@@ -4,7 +4,15 @@ output="unknown"
 if [ -f "/etc/arch-release" ]; then
   output="arch"
 elif [[ -f "/etc/os-release" ]]; then
-  output="ubuntu"
+  # Source the file to load the variables
+  . /etc/os-release
+  if [[ "$ID" == "fedora" ]]; then
+    output="fedora"
+  elif [[ "$ID" == "ubuntu" ]]; then
+    output="ubuntu"
+  elif [[ "$ID" == "debian" ]]; then
+    output="debian2"
+  fi
 elif [[ -f "/etc/debian_version" ]]; then
   output="debian"
 fi
