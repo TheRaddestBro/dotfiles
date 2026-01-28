bc3c2c4504b4a0f1467acd626ce718f54417bcea
Robert Hunt <rohun@ROHUN-GIGAN.localdomain>
2026-01-28 09:46:25 -0800

    Adding helper to obtain Distro name for use in my other scripts

---
+++ helpers/_getDistro.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+output="unknown"
+if [ -f "/etc/arch-release" ]; then
+  output="arch"
+elif [[ -f "/etc/debian_version" ]]; then
+  output="debian"
+fi
+
+echo $output
+
+# If no distro was found, exit with an error
+if [[ "$output" == "unknown" ]]; then
+  exit 1
+else
+  exit 0
+fi
