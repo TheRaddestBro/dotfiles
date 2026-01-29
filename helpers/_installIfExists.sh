57bcb809d2ab93a14ac3ba88e6f195ff9f307df6
Robert Hunt <rohun@ROHUN-GIGAN.localdomain>
2026-01-29 10:16:29 -0800

    Adding --needed to pacman installs

--- helpers/_installIfExists.sh
+++ helpers/_installIfExists.sh
@@ -32,5 +32,5 @@ if [[ "$distroName" == "debian" || "$distroName" == "ubuntu" ]]; then
     sudo apt install -y $final_string
   fi
 elif [[ "$distroName" == "arch" ]]; then
-  pacman -S --noconfirm $final_string
+  pacman -S --needed --noconfirm $final_string
 fi
