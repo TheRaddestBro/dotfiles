d63e0fbdd2ed3861f684e65b84fd5a53447275b5
Robert Hunt <rhunt@CHUNGUS.localdomain>
2026-01-30 15:36:16 -0800

    Fixing vi alias for Arch distros

--- nvim/setupNvimAlternatives.sh
+++ nvim/setupNvimAlternatives.sh
@@ -3,8 +3,12 @@ echo "Setting up nvim as an alternative for vi and vim"
 
 CUSTOM_NVIM_PATH=~/.dotfiles/nvim/startNvim.sh
 
-sudo update-alternatives --install /usr/bin/vi vi ${CUSTOM_NVIM_PATH} 60
-sudo update-alternatives --install /usr/bin/vim vim ${CUSTOM_NVIM_PATH} 60
+if [[ "$distroName" == "debian" || "$distroName" == "ubuntu" ]]; then
+  sudo update-alternatives --install /usr/bin/vi vi ${CUSTOM_NVIM_PATH} 60
+  sudo update-alternatives --install /usr/bin/vim vim ${CUSTOM_NVIM_PATH} 60
+elif [[ "$distroName" == "arch" ]]; then
+  echo "Arch does not support update-alternatives. The .aliases file will be responsible for making vi = nvim"
+fi
 
 # The stackoverflow also had these, but this seems to work without them. Omitting for now.
 #sudo update-alternatives --set vi "${CUSTOM_NVIM_PATH}"
