8d7d21c4cebe62e33e625c6c0a6f96516c1e4321
Robert Hunt <rhunt@live.com>
2026-03-23 21:25:07 -0700

    Adding a specific install download for debian GCM

--- git/setupGCM_LinuxDesktop.sh
+++ git/setupGCM_LinuxDesktop.sh
@@ -4,6 +4,18 @@ echo "Installing GCM from binary."
 helpersPath="../helpers"
 distroName=$(./${helpersPath}/_getDistro.sh)
 
+if [[ "$distroName" == "debian" || "$distroName" == "ubuntu" ]]; then
+  versionString="2.7.3"
+  latestURL="https://github.com/git-ecosystem/git-credential-manager/releases/latest/download"
+  fileName="gcm-linux-x64-${versionString}.deb"
+  #fileName="gcm-linux-arm64-${versionString}.deb"
+
+  wget ${latestURL}/${fileName}
+  sudo dpkg -i ${fileName}
+else
+  echo "${distroName} is not supported.. YET"
+fi
+
 git config --global credential.helper manager
 git config --global credential.credentialStore secretservice
 
