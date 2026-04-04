06e6945c6e5f4a9e35177cdd29a87aa973abf978
Robert Hunt <rohun@ROHUN-SURF-3.localdomain>
2026-04-04 16:01:25 -0700

    Updates to the way the .gitconfig is brought over. Standardized the stows a bit with a common way to deal with existing files

--- setupStows.sh
+++ setupStows.sh
@@ -4,10 +4,27 @@
 # This helps with other stows wanting to put things in this same folder tree.
 mkdir -p $HOME/.config/
 
-# Git needs some special attention, as the ~/.gitconfig might already exist, and we don't want to overwrite it.
-pushd git
-./setupBasic.sh
-popd
+
+function renameOldFile {
+  local file=$1
+  local oldFileName=${file}_OLD
+
+  if [ -L ${file} ]; then
+    echo "${file} already existed as a symlink. I'm going to leave it alone..."
+    return 0
+  fi
+  
+  if [ -f ${file} ]; then
+    echo "${file} already existed. Renaming to ${oldFileName} just in case you might need it."
+    mv ${file} ${oldFileName}
+  fi
+  
+  return 0
+}
+
+# If any of these files already existed, rename them so they aren't totally list in case of a fire or whatever.
+renameOldFile "${HOME}/.bash_profile"
+renameOldFile "${HOME}/.gitconfig"
 
 ./_justTheStows.sh
 
