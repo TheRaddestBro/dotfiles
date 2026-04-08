6eecd5151458ba5f6abd75e64d59db7491cbb97d
Robert Hunt <rohun@ROHUN-GIGAN.localdomain>
2026-04-08 15:14:51 -0700

    Upgrading the set_editor script to look for nvim, vim, vi, and nano. In that order

--- omz/.oh-my-zsh/custom/set_editor.zsh
+++ omz/.oh-my-zsh/custom/set_editor.zsh
@@ -1,3 +1,17 @@
-export EDITOR=$(which vim)
-export VISUAL=$(which vim)
+function trySetEditor {
+  local desiredEditor=$1
+  editorLocation=$(command -v ${desiredEditor} 2>/dev/null)
+  if [[ -z "$editorLocation" ]]; then
+    #echo "Editor Location NOT found for: $desiredEditor"
+    return 1
+  else
+    #echo "Editor Location found: $editorLocation"
+    export EDITOR=$editorLocation
+    export VISUAL=$editorLocation
+  fi
+
+  return 0
+}
+
+trySetEditor 'nvim' || trySetEditor 'vim' || trySetEditor 'vi' || trySetEditor 'nano'
 
